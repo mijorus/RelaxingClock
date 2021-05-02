48441bb339b7dc9dbcf93beeed0a4411ee6a9fa7
mijorus <lorenzo.mijorus@gmail.com>
2021-05-02 19:19:38 +0200

    added eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    },
+};
