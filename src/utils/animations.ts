66ee9213707c57a336b67f071e24acaf27eba3ea
mijorus <lorenzo.mijorus@gmail.com>
2021-05-02 19:21:14 +0200

    linting

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,7 +1,7 @@
-import { styleChangeLock } from "../stores/globalState";
+import { styleChangeLock } from '../stores/globalState';
 
-export const cbDefault: string = 'cubicBezier(0.37, 0, 0.63, 1)';
-export const eaElasticDefault: string = 'spring(1, 60, 10, 0)';
+export const cbDefault = 'cubicBezier(0.37, 0, 0.63, 1)';
+export const eaElasticDefault = 'spring(1, 60, 10, 0)';
 
 export function lockApp() {
     styleChangeLock.set(true);
