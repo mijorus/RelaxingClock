dbcab7f71b487215988824f606b314082003c720
mijorus <lorenzo.mijorus@gmail.com>
2021-05-16 12:45:49 +0200

    import activeStle from custom Store

--- src/components/elements/StyleSelectionBox.svelte
+++ src/components/elements/StyleSelectionBox.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { spring } from 'svelte/motion';
-    import { activeStyle } from "../../stores/clockStyle";
+    import { activeStyle } from "../../stores/settings";
     import { screenSaver, styleChangeLock } from '../../stores/globalState';
     import styles from "../clock/clockStyles/styles";
 
