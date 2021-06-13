6303d8281d0344f295dfd2b3d28131ba4c561246
lorenzo paderi <>
2021-06-13 18:21:54 +0200

    show icon in checkbox

--- src/components/elements/settings/Buttons/Checkbox.svelte
+++ src/components/elements/settings/Buttons/Checkbox.svelte
@@ -13,7 +13,7 @@
 
 <span on:click={dispatchEvent} class="{hasFocus ? 'in-focus' : ''} inline-block">
     <input type="checkbox" class="w-0 h-0 opacity-0 absolute" bind:checked={checked} on:focus={() => hasFocus = true} on:blur={() => hasFocus = false}>
-    <i class="lnr lnr-checkmark-circle text-primary mr-2 cursor-pointer {checked ? 'opacity-100' : 'opacity-50'}"></i>
+    <i class="lnr text-primary mr-2 cursor-pointer {checked ? 'lnr-checkmark-circle opacity-100' : 'lnr-circle-minus opacity-50'}"></i>
 </span>
 
 <style>
