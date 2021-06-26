153e91c6227801a412bc5db7494c702a00d1203d
mijorus <>
2021-06-26 11:55:09 +0200

    support for foldable nested blocks

--- src/components/elements/settings/NestedBox.svelte
+++ src/components/elements/settings/NestedBox.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+    import { slide } from "svelte/transition";
     export let label: string;
-    export let available: boolean = true;
-    export let bordered: boolean = true;
+    export let available = true;
+    export let bordered = true;
+    export let expandable = false;
 
+    let expanded = !expandable;
 </script>
 
 <div class="w-full md:w-9/12 bg-secondary rounded-xl p-3 md:p-4 m-3 md:m-4 self-end
     {available ? 'opacity-100' : 'opacity-50 pointer-events-none'} 
     {bordered ? 'border-l-4 border-primary' : 'border-none'}"
 >
-    <span class="text-primary font-primary text-md">{label}</span>
-    <slot></slot>
+    <div class="text-primary font-primary text-md w-full flex items-center" style="justify-content: space-between;">
+        <span>{label}</span>
+        {#if expandable}<i class="fas fa-chevron-{expanded ? 'up' : 'down'} cursor-pointer" on:click={() => expanded = !expanded}></i>{/if}
+    </div>
+    <div class="unfold max-h-80">
+        {#if expanded }
+            <div in:slide out:slide><slot></slot></div>
+        {/if}
+    </div>
 </div>
