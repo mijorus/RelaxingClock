354766a59ed2ad6982a48e0ff13e67d6c7f67a10
mijorus <>
2021-07-03 14:32:30 +0200

    removed init handler

--- src/handlers/init.ts
+++ src/handlers/init.ts
@@ -1,10 +1,18 @@
-import { get } from 'svelte/store';
+// import { get } from 'svelte/store';
+// import Spotify from 'spotify-web-api-js'
 
-import { attemptSpotifyLogin } from './spotify/login';
-import { onlineStatus } from '../stores/globalState';
+// import { attemptSpotifyLogin } from './spotify/login';
+// import { onlineStatus } from '../stores/globalState';
+// import { spotifyPlayerStatus } from '../stores/spotify';
 
-export default function init() {
-    if (get(onlineStatus)) {
-        attemptSpotifyLogin();
-    }
-}
+// export default function init() {
+//     if (get(onlineStatus)) {
+//         // attemptSpotifyLogin();
+//         unSub = spotifyPlayerStatus.subscribe(status => {
+//             if (status === 'ready') {
+//                 const s = new Spotify()
+//                 s.setAccessToken('')
+//             }
+//         })
+//     }
+// }
