ad28b6d0a253685dd539a6e4664ef3ff4ff8ac21
mijorus <>
2021-07-03 14:33:10 +0200

    added some spotify status store

--- src/stores/spotify.ts
+++ src/stores/spotify.ts
@@ -2,4 +2,6 @@ import { Writable, writable } from 'svelte/store';
 import type { SpotifyPlayerStatus } from '../types';
 
 export const spotifyUrl: Writable<string> = writable(undefined);
+export const spotifyAccessToken: Writable<string> = writable(undefined);
 export const spotifyPlayerStatus: Writable<SpotifyPlayerStatus> = writable('disconnected');
+export const spotifyUserData: Writable<SpotifyApi.CurrentUsersProfileResponse> = writable(undefined);
