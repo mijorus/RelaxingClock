8d1a54290554c19c0ded7a116333bc25cd311a4e
mijorus <>
2021-07-17 17:29:53 +0200

    added sleep util

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -48,3 +48,7 @@ export function groupBy(array: Array<any>, key: string) {
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
