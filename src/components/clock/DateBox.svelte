30d598e1e5cd16051c132a6e6f0b38e79dfe296a
mijorus <>
2021-08-18 17:40:53 +0200

    fixed datebox showing n of week

--- src/components/clock/DateBox.svelte
+++ src/components/clock/DateBox.svelte
@@ -6,5 +6,5 @@ import { fly, fade } from 'svelte/transition';
 <div class="absolute text-xl text-center text-bg left-2/4 bottom-4 transform -translate-x-1/2" 
     in:fly={{'y': 40, 'duration': 1000}} 
     out:fade={{'duration': 50}}>
-    <span class="bg-highlighted rounded-full inline-block px-2 py-1 -top-10">{$time.format('dddd, d MMMM')}</span>
+    <span class="bg-highlighted rounded-full inline-block px-2 py-1 -top-10">{$time.format('dddd, D MMMM')}</span>
 </div>
