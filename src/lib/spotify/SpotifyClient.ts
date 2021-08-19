e8bb4514a9a9c7bc0f23a6110f8a9530f2100d8b
mijorus <>
2021-08-19 18:37:07 +0200

    test wrapper

--- src/lib/spotify/SpotifyClient.ts
+++ src/lib/spotify/SpotifyClient.ts
@@ -1,3 +1,28 @@
+import SpotifyWebApi from 'spotify-web-api-js';
 import Spotify from 'spotify-web-api-js'
+import { attemptSpotifyLogin } from '../../handlers/spotify/login';
 
-export const SpotifyClient = new Spotify();
+
+const sp = new Spotify();
+class S {
+    constructor() {
+        const handler = {
+            get: function (obj, prop) {
+                if (typeof obj[prop] !== "function") {
+                    return obj[prop];
+                } else { 
+                    return function (...args) {
+                        return obj[prop].apply(obj, args)?.catch((err) => {
+                            attemptSpotifyLogin()
+                                .catch(err => { throw err });
+                        });  
+                    };
+                }
+            }
+        }
+
+        return new Proxy(sp, handler);
+    }
+}
+
+export const SpotifyClient: SpotifyWebApi = new S();
