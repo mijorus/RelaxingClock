a3a3d00119e96d766428f81ab267a15a5d5efdc8
mijorus <>
2021-09-01 15:48:15 +0200

    use timeout to refresh token

--- src/lib/spotify/SpotifyClient.ts
+++ src/lib/spotify/SpotifyClient.ts
@@ -2,6 +2,3 @@ import Spotify from 'spotify-web-api-js'
 import time from '../../stores/time';
 
 export const SpotifyClient = new Spotify();
-export let tokenTtl: number = undefined;
-
-export function setTokenTtl(date: number) { tokenTtl = date }
