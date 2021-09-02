389e3e9715a8355aaa738be86b0acbcd99350619
mijorus <>
2021-09-02 18:17:48 +0200

    update weather widget every once in a while

--- src/components/sections/WeatherWidget.svelte
+++ src/components/sections/WeatherWidget.svelte
@@ -4,19 +4,20 @@ import { fade } from "svelte/transition";
 import type { Hourly } from "../../lib/openweathermap/client";
 import { mainWeatherConditions } from "../../lib/openweathermap/mainConditions";
 import { lastWeatherUpdate, weather } from "../../stores/storedSettings";
+import time from "../../stores/time";
 
     let data: Hourly[] = [];
     let icon: string;
     let customLocation: string[];
 
     $:  {
-        if ($lastWeatherUpdate?.hourly) {
-            data = [];
+        if ($weather && $lastWeatherUpdate?.hourly && ( !data.length || ($time.minutes() % 20 === 0) ) ) {
+            console.log('updating weather widget'); data = [];
             for (let index = 0; index < 24; index++) data.push(null);
                 
             $lastWeatherUpdate.hourly.forEach((el: Hourly) => {
                 const date = moment(el.dt, 'X');
-                if (date.isSame(moment(), 'day')) data[date.hours()] = el;
+                if (date.isSame(moment(), 'day') && date.isSameOrAfter(moment())) data[date.hours()] = el;
             });
 
             icon = mainWeatherConditions[data.find(e => e !== null).weather[0].main]?.icon;
@@ -28,8 +29,7 @@ import { lastWeatherUpdate, weather } from "../../stores/storedSettings";
 {#if $weather}
     <div class="relative flex flex-row items-center" transition:fade>
         {#if data.length}
-            {#each data as d, i}{#if !d}<span class="w-3 h-1 inline-block" style="background-color: rgb(63, 63, 63);" ></span>{/if}{/each}
-            {#if icon}<img class="inline-block w-16" src="/media/{icon}" alt=""/>{/if}{#each data as d, i}{#if d}<span class="w-3 h-1 inline-block p-0 m-0" style="background-color: {mainWeatherConditions[d.weather[0].main].color};"></span>{/if}
+            {#each data as d, i}{#if !d}<span class="w-3 h-1 inline-block" style="background-color: rgb(63, 63, 63);" class:rounded-l-xl={i === 0}></span>{/if}{/each}{#if icon}<img class="inline-block w-16" src="/media/{icon}" alt=""/>{/if}{#each data as d, i}{#if d}<span class="w-3 h-1 inline-block p-0 m-0" style="background-color: {mainWeatherConditions[d.weather[0].main].color};" class:rounded-r-xl={i === (data.length - 1)}></span>{/if}
             {/each}
         {/if}
     </div>
@@ -37,3 +37,4 @@ import { lastWeatherUpdate, weather } from "../../stores/storedSettings";
         <p class="text-center text-secondary">{customLocation[3]}, {customLocation[2]}</p>
     {/if}
 {/if}
+
