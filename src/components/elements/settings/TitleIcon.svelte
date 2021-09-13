0ee568c0b098bdc5a1898258c7c5735325fb8b2e
mijorus <>
2021-09-13 21:48:20 +0200

    added Settings section

--- src/components/elements/settings/TitleIcon.svelte
+++ src/components/elements/settings/TitleIcon.svelte
@@ -1,3 +1,3 @@
-<span class="absolute -left-10 bottom-2/4 transform -translate-x-2/4 translate-y-2/4 text-5xl">
+<span class="absolute -left-10 bottom-2/4 transform -translate-x-2/4 translate-y-2/4 text-5xl bg-dark rounded-full">
     <slot></slot>
 </span>
