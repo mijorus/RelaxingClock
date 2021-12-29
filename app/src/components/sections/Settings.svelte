d66a2d479186ce4be4d0d0b924a7cf5c1ee07774
mijorus <>
2021-12-29 14:35:10 +0100

    set cursor pointer class to settings clock

--- app/src/components/sections/Settings.svelte
+++ app/src/components/sections/Settings.svelte
@@ -41,7 +41,7 @@ import ShowTutorial from './settingsPage/ShowTutorial.svelte';
     </div>
     {#if !$clockIsVisible}
         <div class="p-4 fixed bottom-0 left-0 xl:text-7xl opacity-50 lg:text-5xl md:text-4xl hidden md:block font-clock text-primary" transition:slide>
-            <a href="#big-clock-container"><Hours interactive={false}/><Divisor /><Minutes /></a>
+            <a href="#big-clock-container" class="cursor-pointer"><Hours interactive={false}/><Divisor /><Minutes /></a>
         </div>
     {/if}
 </div>
