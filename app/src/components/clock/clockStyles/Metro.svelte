7103b2bcfd984faed14c32638182bdb14e2b781b
mijorus <>
2022-01-02 00:11:34 +0100

    added events indicatorns in metro style

--- app/src/components/clock/clockStyles/Metro.svelte
+++ app/src/components/clock/clockStyles/Metro.svelte
@@ -11,9 +11,11 @@ import Minutes from '../Minutes.svelte';
 import StyleBase from './StyleBase.svelte';
 import { blink } from '../../../stores/storedSettings';
 import AmPmBadge from '../AmPmBadge.svelte';
+import IncomingEventsBox from '../IncomingEventsBox.svelte';
     
     let container: HTMLElement;
     const zoomedOut = 0.7;
+    let isHovered = false;
     const shiftedUp = '-20%';
     $: toggleScreenSaver($screenSaver);
 
@@ -31,6 +33,14 @@ import AmPmBadge from '../AmPmBadge.svelte';
 
 <StyleBase styleId={3}>
         <div bind:this={container} class="flex flex-col" style="transform: scale({zoomedOut * 100}%) translateY({shiftedUp});">
+            {#if $screenSaver}
+                <div class="absolute text-base w-full top-5 z-20" on:mouseenter={() => isHovered = true} on:mouseleave={() => isHovered = false}>
+                    <IncomingEventsBox {isHovered} />
+                </div>
+                <!-- <div class="absolute left-1/2 transform -translate-x-2/4">
+                    <IncomingEventsMessages />
+                </div> -->
+            {/if}
             <Hours />
             <div class="font-clock text-primary text-giant-1 absolute top-1/2 left-1/2 transform -translate-x-2/4 -translate-y-2/4">
                 {#if (($time.second() % 2) && $blink)}&middot{:else}&nbsp;{/if}
