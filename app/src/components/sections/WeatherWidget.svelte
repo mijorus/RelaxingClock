f07402dccf4fc6a03f0f089b503a47297d345b94
mijorus <>
2022-01-02 00:14:11 +0100

    reworked weather widget to fit the new icons

--- app/src/components/sections/WeatherWidget.svelte
+++ app/src/components/sections/WeatherWidget.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import moment from "moment";
+import moment from "moment-timezone"
 import { fade } from "svelte/transition";
 import type { Hourly } from "../../lib/openweathermap/client";
 import { getConditionIcon, mainWeatherConditions } from "../../lib/openweathermap/mainConditions";
@@ -25,21 +25,21 @@ import WeatherModal from "../modals/WeatherModal.svelte";
             data = [];
             for (let index = 0; index < 24; index++) data.push(null);
                 
-            $lastWeatherUpdate.hourly.sort((a, b) => a.dt - b.dt).forEach((el: Hourly) => {
-                const date = moment(el.dt, 'X');
-                if (date.isSame(moment(), 'day') && date.isSameOrAfter(moment())) data[date.hours()] = el;
+            $lastWeatherUpdate.hourly.forEach((el: Hourly) => {
+                const momentTZ = moment().tz($lastWeatherUpdate.timezone);
+                const date = moment(el.dt, 'X').tz($lastWeatherUpdate.timezone);
+                if (date.isSame(momentTZ, 'day') && date.isSameOrAfter(momentTZ)) data[date.hours()] = el;
             });
 
             const currentWeather: WeatherCondition = $lastWeatherUpdate.current.weather[0];
             const momentSunset = moment($lastWeatherUpdate.current.sunset, 'X');
             const momentSunrise = moment($lastWeatherUpdate.current.sunrise, 'X');
             
+            isNight = false;
+            icon = getConditionIcon(currentWeather).icon ?? undefined;
             if ((moment().isSameOrAfter(momentSunset) || moment().isBefore(momentSunrise)) && getConditionIcon(currentWeather).icon_night) {
                 isNight = true;
                 icon = getConditionIcon(currentWeather).icon_night;
-            } else {
-                isNight = false;
-                icon = getConditionIcon(currentWeather).icon ?? undefined;
             }
 
             currentLocation = localStorage.getItem('currentLocation') ? localStorage.getItem('currentLocation').split(',') : null;
@@ -58,9 +58,9 @@ import WeatherModal from "../modals/WeatherModal.svelte";
 </script>
 
 {#if $weather}
-    <div class="relative flex flex-row items-center" transition:fade on:mouseenter={() => { showWeatherTips()}} on:mouseleave={() => tips.set(null)}>
+    <div class="relative flex flex-row items-center mt-5 mb-7" transition:fade on:mouseenter={() => { showWeatherTips()}} on:mouseleave={() => tips.set(null)}>
         {#if data.length}
-            {#each data as d, i}{#if !d}<span class="w-3 h-1 inline-block overlap" style="background-color: rgb(63, 63, 63);" class:rounded-l-xl={i === 0}></span>{/if}{/each}{#if icon}<img class="inline-block w-16" src="/media/{icon}" alt=""/>{/if}{#each data as d, i}{#if d}<span class="w-3 h-1 inline-block p-0 overlap" style="background-color: {isNight && mainWeatherConditions[d.weather[0].main].color_night ? mainWeatherConditions[d.weather[0].main].color_night : mainWeatherConditions[d.weather[0].main].color};" class:rounded-r-xl={i === (data.length - 1)}></span>{/if}
+            {#each data as d, i}{#if !d}<span class="w-3 h-1 inline-block overlap bg-secondary" class:rounded-l-xl={i === 0}></span>{/if}{/each}<span class="inline-block w-16 h-1 bg-highlighted relative rounded-xl">{#if icon}<img class="absolute left-1/2 transform -translate-x-2/4 -translate-y-2/4 inline-block w-16 hover:scale-125 transition-transform" src="/media/{icon}" alt=""/>{/if}</span>{#each data as d, i}{#if d}<span class="w-3 h-1 inline-block p-0 overlap" style="background-color: {isNight && mainWeatherConditions[d.weather[0].main].color_night ? mainWeatherConditions[d.weather[0].main].color_night : mainWeatherConditions[d.weather[0].main].color};" class:rounded-r-xl={i === (data.length - 1)}></span>{/if}
             {/each}
         {/if}
     </div>
