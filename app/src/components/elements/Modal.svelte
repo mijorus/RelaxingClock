85f981b5b4991c33e6cf665341b1b79db1b27db4
mijorus <lorenzo.mijorus@gmail.com>
2022-01-14 01:23:46 +0100

    close modal on exit

--- app/src/components/elements/Modal.svelte
+++ app/src/components/elements/Modal.svelte
@@ -7,6 +7,7 @@ import { modalContent } from "../../stores/globalState";
     }
 </script>
 
+<svelte:window on:keydown={(e) => {if (e.key === 'Escape' && $modalContent) e.stopPropagation(); closeModal(e); }} />
 {#if $modalContent}
     <div class="fixed flex flex-col justify-center items-center top-0 px-5 left-0 w-screen h-screen bg-black bg-opacity-40" on:click={closeModal} transition:fade style="z-index: 49;">
         <div class="relative text-primary p-4 bg-primary rounded-xl w-full h-4/5 m-5 md:w-2/3 md:h-2/3 overflow-hidden" on:click|stopPropagation >
