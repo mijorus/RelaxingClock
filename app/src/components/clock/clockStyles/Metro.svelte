858c75b94735b7eab0ea2e35102d8f5430d2ecf2
mijorus <>
2022-02-07 22:23:11 +0100

    added incoming to metro clock

--- app/src/components/clock/clockStyles/Metro.svelte
+++ app/src/components/clock/clockStyles/Metro.svelte
@@ -12,6 +12,7 @@ import StyleBase from './StyleBase.svelte';
 import { blink } from '../../../stores/storedSettings';
 import AmPmBadge from '../AmPmBadge.svelte';
 import IncomingEventsBox from '../IncomingEventsBox.svelte';
+import IncomingEventsMessages from '../IncomingEventsMessages.svelte';
     
     let container: HTMLElement;
     const zoomedOut = 0.7;
@@ -37,14 +38,14 @@ import IncomingEventsBox from '../IncomingEventsBox.svelte';
                 <div class="absolute text-base w-full top-5 z-20" on:mouseenter={() => isHovered = true} on:mouseleave={() => isHovered = false}>
                     <IncomingEventsBox {isHovered} />
                 </div>
-                <!-- <div class="absolute left-1/2 transform -translate-x-2/4">
-                    <IncomingEventsMessages />
-                </div> -->
             {/if}
             <Hours />
             <div class="font-clock text-primary text-giant-1 absolute top-1/2 left-1/2 transform -translate-x-2/4 -translate-y-2/4">
                 {#if (($time.second() % 2) && $blink)}&middot{:else}&nbsp;{/if}
             </div>
             <Minutes />
+            <div class="{$screenSaver ? 'absolute' : 'hidden'} left-1/2 transform -translate-x-2/4 top-full">
+                <IncomingEventsMessages />
+            </div>
         </div>
 </StyleBase>
