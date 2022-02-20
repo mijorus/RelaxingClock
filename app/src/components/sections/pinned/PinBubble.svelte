1d024907d028a781cc37b1f830a67e529b3ec292
mijorus <>
2022-02-20 17:00:21 +0100

    improved tips on pin bubble hover

--- app/src/components/sections/pinned/PinBubble.svelte
+++ app/src/components/sections/pinned/PinBubble.svelte
@@ -5,13 +5,24 @@ import { fade, scale } from "svelte/transition";
 import colors  from "simple-color-functions";
 import Pin from "../../icons/Pin.svelte";
 import AnimatedText from "../../elements/AnimatedText.svelte";
+import { tips } from "../../../stores/globalState";
 
     export let p: StoredPinned;
     export let scrollPaused;
     let dispatch = createEventDispatcher();
+
+    function handleMouseEnter() {
+        tips.set([{shortcut: 'Long press pin and move', name: 'Drag the bubble around'}])
+    }
+
+    function handleMouseLeave() {
+        tips.set(null);
+    }
 </script>
 
 <div id="pinned-{p.id}" data-id={p.id} class="hidden md:block absolute top-0 left-0 pinned" on:mousedown|stopPropagation={() => dispatch('focus')}
+    on:mouseenter={handleMouseEnter}
+    on:mouseleave={handleMouseLeave}
     style="transform: translateY({p.top > 0 ? p.top :  0}px) translateX({p.left > 0 ? `${p.left}px` : '0'});"
 >
     <div class="pinned-inner bg-black relative m-2 xl:m-6 rounded-2xl p-0 text-primary w-80" transition:scale>
