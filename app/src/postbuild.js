751662a0ce197f4c7e14ee956bc5be40bbd8bd42
mijorus <>
2022-02-28 09:36:31 +0100

    WIP

--- app/src/postbuild.js
+++ app/src/postbuild.js
@@ -5,7 +5,7 @@ const manifest = require('./_manifest.json');
 manifest.version = packageConfig.version;
 manifest.start_url = process.env.SPOTIFY_REDIRECT_URL;
 
-fs.writeFile(__dirname + './../../public/manifest.json', JSON.stringify(manifest), err => {
+fs.writeFile(__dirname + '/../../public/manifest.json', JSON.stringify(manifest), err => {
     if (err) {
         console.error(err);
         process.exit(1);
--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,10 @@ self.addEventListener('install', event => {
     // Do install stuff
 });
 
+self.addEventListener('fetch', event => {
+    // Do install stuff
+});
+
 self.addEventListener('activate', event => {
     // Do activate stuff
 });
