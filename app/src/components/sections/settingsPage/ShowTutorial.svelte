ddb90c30c49539c6b05d3a915817d75e75997b18
mijorus <>
2022-11-07 10:04:02 +0100

    fix typo

--- app/src/components/sections/settingsPage/ShowTutorial.svelte
+++ app/src/components/sections/settingsPage/ShowTutorial.svelte
@@ -19,7 +19,7 @@
     <PrimaryBox label={{ text: "Show the introduction tutorial again" }} description={{ text: "Just in case you missed something...", iconClass: "lnr lnr-question-circle" }} available={true}>
         <Action label="Open" on:click={() => modalContent.set(IntroTutorialModal)} />
     </PrimaryBox>
-    <PrimaryBox label={{ text: "Latest news" }} description={{ text: "Check what\'s new with te latest version", iconClass: "lnr lnr-question-circle" }} available={true}>
+    <PrimaryBox label={{ text: "Latest news" }} description={{ text: "Check what\'s new with the latest version", iconClass: "lnr lnr-question-circle" }} available={true}>
         <Action label="Open" on:click={() => modalContent.set(UpdateModal)} />
     </PrimaryBox>
     <PrimaryBox label={{ text: "Take a survey" }} description={{ text: "Help us imporving Relaxing Clock", iconClass: "lnr lnr-question-circle" }} available={true}>
