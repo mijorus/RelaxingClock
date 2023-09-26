aef8dcb792eba1d7c2a052c2b60c0fa6c41a6423
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2023-09-26 23:36:07 +0200

    refresh bing images every day

--- app/src/components/sections/settingsPage/BackgroundImage.svelte
+++ app/src/components/sections/settingsPage/BackgroundImage.svelte
@@ -8,10 +8,15 @@
     import { bgImageBright } from "../../../stores/globalState";
     import axios from "axios";
     import Action from "../../elements/settings/Buttons/Action.svelte";
+    import Shuffle from "../../icons/Shuffle.svelte";
+    import { onMount } from "svelte";
 
     let imageReference: HTMLImageElement;
-    let loadingError = false;
+    let loadingStatus: "error" | "loaded" | "loading" | "none" = "none";
+    const bingRefreshKey = "bingWallpaperRefresh";
     const customClass = "bg-transparent border-2 border-white";
+
+    const demoImage = "https://images.unsplash.com/photo-1470115636492-6d2b56f9146d?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1MDc4NTl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTU3NjA0MzR8&ixlib=rb-4.0.3&q=85";
 
     function setImageBrigthness() {
         imageReference.crossOrigin = "Anonymous";
@@ -40,62 +45,102 @@
     }
 
     async function setBingImage() {
-        loadingError = false;
+        loadingStatus = "loading";
+        backgroundImageSource.set("bing");
+
+        if (!process.env.production) {
+            backgroundImage.set(demoImage);
+            loadingStatus = "loaded";
+            return;
+        }
 
         try {
             const response = await axios.get("/.netlify/functions/bingDailyImage");
             backgroundImage.set(response.data.url);
-            backgroundImageSource.set("bing");
+            sessionStorage.setItem(bingRefreshKey, Date.now().toString());
         } catch (e) {
             console.error(e);
-            loadingError = true;
+            loadingStatus = "error";
+            backgroundImage.set('');
         }
+
+        loadingStatus = "loaded";
     }
 
     async function setUnsplashImage() {
-        loadingError = false;
+        loadingStatus = "loading";
+        backgroundImageSource.set("unsplash");
+
+        if (!process.env.production) {
+            backgroundImage.set(demoImage);
+            loadingStatus = "loaded";
+            return;
+        }
 
         try {
             const response = await axios.get("/.netlify/functions/unsplashImage");
-
             backgroundImage.set(response.data.urls.full);
-            backgroundImageSource.set("unsplash");
         } catch (e) {
             console.error(e);
-            loadingError = true;
+            loadingStatus = "error";
+            backgroundImage.set('');
         }
+
+        loadingStatus = "loaded";
     }
 
     function removeBgImage() {
-        loadingError = false;
+        loadingStatus = "none";
 
         backgroundImage.set("");
         backgroundImageSource.set("default");
+        bgImageBright.set("");
     }
+
+    onMount(() => {
+        if ($backgroundImage.length) {
+            loadingStatus = "loaded";
+        }
+
+        if ($backgroundImageSource === "bing" && sessionStorage.getItem(bingRefreshKey) === undefined) {
+            setBingImage();
+        }
+    });
 </script>
 
 <SettingsBox bordered={false} id="background-image">
     <Title title="Background">
         <TitleIcon>
-            <i class="lnr lnr-leaf text-green-500 settings-title-icon" />
+            <i class="lnr lnr-picture text-purple-500 settings-title-icon" />
         </TitleIcon>
     </Title>
     <PrimaryBox
         label={{ text: "Select a style" }}
         description={{
-            text: "Reduce motion and animations when the window in not in focus. Can help if your laptop is running on battery or you want to reduce the impact on your GPU",
+            text: "Customize the background of the clock with images: use Bing to get a new wallpaper every day, while Unsplash provides a full catalog of images to choose from with the Randomize button",
             iconClass: "lnr lnr-question-circle",
         }}
         available={true}
     >
-        {#if loadingError}
-            <span class="inline-block text-red-500 mr-6 rounded-full">
-                <i class="fas fa-exclamation-circle" />
-            </span>
-        {/if}
-        <Action on:click={() => removeBgImage()} custom={$backgroundImageSource !== "default"} customClass={$backgroundImageSource !== "default" ? customClass : ""} label="Default" />
-        <Action on:click={() => setBingImage()} custom={$backgroundImageSource !== "bing"} customClass={$backgroundImageSource !== "bing" ? customClass : ""} label="Bing" />
-        <Action on:click={() => setUnsplashImage()} custom={$backgroundImageSource !== "unsplash"} customClass={$backgroundImageSource !== "unsplash" ? customClass : ""} label="Unsplash" />
+        <div class="flex flex-row items-center gap-2">
+            {#if loadingStatus === "error"}
+                <span class="inline-block text-red-500 mr-6 rounded-full">
+                    <i class="fas fa-exclamation-circle" />
+                </span>
+            {:else if loadingStatus === "loading"}
+                <span class="inline-block text-white animate-spin">
+                    <i class="fas fa-sync-alt" />
+                </span>
+            {:else if loadingStatus === "loaded" && $backgroundImageSource === "unsplash"}
+                <span class="inline-block text-white">
+                    <Shuffle color="#fff" />
+                </span>
+            {/if}
+
+            <Action on:click={() => removeBgImage()} custom={$backgroundImageSource !== "default"} customClass={$backgroundImageSource !== "default" ? customClass : ""} label="Default" />
+            <Action on:click={() => setBingImage()} custom={$backgroundImageSource !== "bing"} customClass={$backgroundImageSource !== "bing" ? customClass : ""} label="Bing" />
+            <Action on:click={() => setUnsplashImage()} custom={$backgroundImageSource !== "unsplash"} customClass={$backgroundImageSource !== "unsplash" ? customClass : ""} label="Unsplash" />
+        </div>
     </PrimaryBox>
 </SettingsBox>
 
