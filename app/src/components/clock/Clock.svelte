aa588ea4fdc256ed7a6988b3a68f7be2a672fa8b
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2023-09-27 13:37:50 +0200

    refactor

--- app/src/components/clock/Clock.svelte
+++ app/src/components/clock/Clock.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-import { windowReady } from 'html-ready';
+    import { windowReady } from "html-ready";
 
-import styles from "./clockStyles/styles";
-import { activeStyle, presentation } from "../../stores/storedSettings";
-import { activeStyleId, nextStyleId } from "../../stores/clockStyle";
-import anime from "animejs";
-import { eaElasticDefault } from "../../utils/animations";
-import { onMount, tick } from 'svelte';
-import { clockIsVisible, styleChangeLock } from '../../stores/globalState';
-import { screenSaver } from '../../stores/globalState';
-import { clockStyleClass } from "../../stores/storedSettings";
+    import styles from "./clockStyles/styles";
+    import { activeStyle, presentation } from "../../stores/storedSettings";
+    import { activeStyleId, nextStyleId } from "../../stores/clockStyle";
+    import anime from "animejs";
+    import { eaElasticDefault } from "../../utils/animations";
+    import { onMount, tick } from "svelte";
+    import { clockIsVisible, styleChangeLock } from "../../stores/globalState";
+    import { screenSaver } from "../../stores/globalState";
+    import { clockStyleClass } from "../../stores/storedSettings";
 
     let bigClock: HTMLElement;
     $: setCurrentPosition($activeStyle);
@@ -17,33 +17,44 @@ import { clockStyleClass } from "../../stores/storedSettings";
     async function setCurrentPosition(activeStyle: number) {
         await windowReady;
         if (activeStyle !== undefined) {
-            const toStyleId: number =  styles[activeStyle].id;
+            const toStyleId: number = styles[activeStyle].id;
 
             if (bigClock) {
                 anime({
-                    begin() { nextStyleId.set(toStyleId); },
+                    begin() {
+                        nextStyleId.set(toStyleId);
+                    },
                     targets: bigClock,
                     duration: 750,
                     easing: eaElasticDefault,
                     translateX: `${activeStyle * (-100 / styles.length)}%`,
-                    complete() { activeStyleId.set(toStyleId) }
-                })
+                    complete() {
+                        activeStyleId.set(toStyleId);
+                    },
+                });
             }
         }
     }
 
     onMount(() => {
         if (!$activeStyle) activeStyle.set(0);
-    })
+    });
 </script>
 
-<div 
+<div
     bind:this={bigClock}
-    id="big-clock" 
-    class="flex flex-row items-center flex-nowrap {$clockStyleClass} font-semibold text-primary text-giant-0.5 lg:text-giant-1  transition-opacity
-        whitespace-nowrap w-auto h-full z-10 absolute top-0 left-0 select-none {($screenSaver && !$presentation) ? 'opacity-75' : ''}"
+    id="big-clock"
+    class="flex flex-row items-center flex-nowrap {$clockStyleClass} font-semibold text-primary text-giant-0.5 lg:text-giant-1 transition-opacity
+        whitespace-nowrap w-auto h-full z-10 absolute top-0 left-0 select-none {$screenSaver && !$presentation ? 'opacity-75' : ''}"
 >
     {#each styles as style}
         <svelte:component this={style.component} />
     {/each}
 </div>
+
+<style global>
+    .common-clock-background-image {
+        opacity: 0.9;
+        text-shadow: 0 0 20px var(--secondary);
+    }
+</style>
