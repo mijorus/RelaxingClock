356c2c44aabcc81f5cfa7b842854da7d3104dceb
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2023-11-08 08:05:10 +0100

    fixed option key showing in windows

--- app/src/costants.ts
+++ app/src/costants.ts
@@ -1,5 +1,3 @@
-// import { isMacintosh } from "./utils/utils";
-
-export const isMacintosh = navigator.userAgent.indexOf('Mac OS X');
+export const isMacintosh = navigator.platform.includes('MacIntel');
 export const altLabel = isMacintosh ? '⌥' : 'Alt';
 export const surveyUrl = 'https://forms.gle/o2CV4tL7R29Np1bk6';
